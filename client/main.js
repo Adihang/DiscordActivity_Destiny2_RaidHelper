76e9dcf42ae9195b4141bb736e6e30887c968997
Adihang <limhan456@naver.com>
2024-08-29 14:49:31 +0900

    구경계산기

--- client/main.js
+++ client/main.js
@@ -1,9 +1,6 @@
 // Import the SDK
 import { DiscordSDK } from "@discord/embedded-app-sdk";
 
-import "./style.css";
-import rocketLogo from '/rocket.png';
-
 // Instantiate the SDK
 const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
 
@@ -14,10 +11,3 @@ setupDiscordSdk().then(() => {
 async function setupDiscordSdk() {
   await discordSdk.ready();
 }
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <img src="${rocketLogo}" class="logo" alt="Discord" />
-    <h1>Hello, World!</h1>
-  </div>
-`;
